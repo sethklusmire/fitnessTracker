ba4e25122bb6bb5f9848960479bf9c372ecabe87
Seth Klusmire <sethklusmire@gmail.com>
2021-02-03 23:07:12 -0700

    routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,43 @@
 const router = require("express").Router();
-const workout = require("../models/workout.js")
+const workout = require("../models/workout.js");
+const Path = require("path")
+
+router.get("/", (req, res) => {
+  res.sendFile(Path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(Path.join(__dirname, "../public/exercise.html"));
+})
+
+router.get("/stats", (req, res) => {
+    res.sendFile(Path.join(__dirname, "../public/stats.html"));
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
